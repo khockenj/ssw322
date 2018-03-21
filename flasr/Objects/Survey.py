008aeffb8333efb2dfdd53082083a975f5d22602
yborai <yborai@stevens.edu>
2018-03-20 22:46:02 -0400

    Added basic Survey functionality

--- flasr/Objects/Survey.py
+++ flasr/Objects/Survey.py
@@ -1,1 +1,17 @@
 """Main Survey Object"""
+
+class Survey:
+	isTest = False
+	answers = None
+    questions = []
+
+    def Survey(isTest):
+        self.isTest = isTest
+
+    def addQuestion(question):
+    	questions.append(question)
+
+    def getQuestionList:
+    	return questions
+
+	
