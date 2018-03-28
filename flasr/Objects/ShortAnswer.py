e8b0ca4bcc381e7ac9c40f3c49738a11875ef063
Rob Weiss <rweiss55@gmail.com>
2018-03-27 23:32:32 -0400

    Resolved Merge Conflict

--- flasr/Objects/ShortAnswer.py
+++ flasr/Objects/ShortAnswer.py
@@ -2,14 +2,16 @@ from .Question import Question
 
 class ShortAnswer(Question):
 
-    def __init__(self, uniqueID, question, response):
+    def __init__(self, uniqueID, question, charLimit):
         self.uniqueID = uniqueID
         self.question = question
+        self.charLimit = charLimit
 
     """def serialize(self):
     	response = {
     	    'uniqueID': self.uniqueID,
     	    'Question': self.question
+            'charLimit': self.charLimit
     	}
 
     	return response
