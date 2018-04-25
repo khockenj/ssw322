7a153585f4690bfbcbdd8c88c05d8288afc40a0a
robjweiss <rweiss55@gmail.com>
2018-04-25 09:44:24 -0400

    No longer broken

--- flasr/Utils/load_survey.py
+++ flasr/Utils/load_survey.py
@@ -11,45 +11,45 @@ from Objects.MultipleChoice import MultipleChoice
 from Objects.ShortAnswer import ShortAnswer
 
 def load_survey(name, db, col):
-    serialized_survey = col.find_one({"name": name})
-    survey_dict = pickle.loads(serialized_survey)
-    questions = survey_dict.questions
-    survey = Survey(survey_dict.isTest)
-    survey.name = name
-    if survey.isTest == True:
-    	survey.answers = survey_dict.answers
+	serialized_survey = col.find_one({"name": name})
+	survey_dict = pickle.loads(serialized_survey)
+	questions = survey_dict.questions
+	survey = Survey(survey_dict.isTest)
+	survey.name = name
+	if survey.isTest == True:
+		survey.answers = survey_dict.answers
 
-    for c in range(len(questions)):
-        survey.addQuestion(create_question(survey, survey_dict.questions[c], survey_dict.isTest))
+	for c in range(len(questions)):
+		survey.addQuestion(create_question(survey, survey_dict.questions[c], survey_dict.isTest))
 
-    return(survey)
+	return(survey)
 
 
 def create_question(survey, question, isTest):
 	if question.q_type == "MC":
-        response = MultipleChoice("MC", question.question)
+		response = MultipleChoice("MC", question.question)
 
-        for c in range(len(question.choices)):
-            response.addChoice(question.choices[c])
+		for c in range(len(question.choices)):
+			response.addChoice(question.choices[c])
 
-    elif question.q_type == "SA":
-        response = ShortAnswer('SA', question.question, question.charLimit)
+	elif question.q_type == "SA":
+		response = ShortAnswer('SA', question.question, question.charLimit)
 
-    elif question.q_type == "R":
-        response = Ranking("R", question.question)
+	elif question.q_type == "R":
+		response = Ranking("R", question.question)
 
-        for c in range(len(question.choices)):
-            response.addChoice(question.choices[c])
+		for c in range(len(question.choices)):
+			response.addChoice(question.choices[c])
 
-    elif question.q_type == "TF":
-        response = TrueFalse("TF", question.question)
+	elif question.q_type == "TF":
+		response = TrueFalse("TF", question.question)
 
-    elif question.q_type == "M":
-        response = Matching("M", question.question)
-        for c in range(len(question.choices)):
-            response.addChoiceAndMatch(question.choices[c], question.matches[c])
+	elif question.q_type == "M":
+		response = Matching("M", question.question)
+		for c in range(len(question.choices)):
+			response.addChoiceAndMatch(question.choices[c], question.matches[c])
 
-    else:
-        return None
+	else:
+		return None
 
-    return response
+	return response
